5cf1e09d9cfb409e63d34cb75cb447f7b45af845
Ester <estervitoriaquerinodasilva2008@gmail.com>
2024-08-28 17:45:14 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+# continuando na linha de baixo
+print("Ester") # comentário a linha do código
+print('Ester') 
+print('Ester "Té" Vitória')
+print("Ester 'Té' Vitória") 
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
