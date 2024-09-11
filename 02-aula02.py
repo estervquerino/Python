a0e5d5f5f64e4fa4153003bf4fa604f2003c2969
Ester <estervitoriaquerinodasilva2008@gmail.com>
2024-09-11 17:29:19 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Ester"
+idade = 16
+altura = 1.70
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, " altura: ", altura)
+# formação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
