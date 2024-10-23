73f08b582410a7195960116be997363bfe7d01a5
Ester <estervitoriaquerinodasilva2008@gmail.com>
2024-10-23 17:14:56 -0300

    Add files via upload

---
+++ 25-aula25.py
@@ -0,0 +1,19 @@
+# Função para verificar se um número é primo
+def e_primo(numero):
+    if numero <=1:
+        return False # Número menores ou iguais a 1 não são primos
+    for i in range(2, int(numero ** 0.5)+ 1):
+        if numero % i == 0:
+            return False # Se o número for divisível por i, não é primo
+    return True # Se não houver divisores, o número é primo
+
+# Solicita um número ao usuário
+num = int(input("Digite um número:  "))
+
+# Verifica se o número é primo e exibe ao usuário
+if e_primo(num):
+    print(f"{num} é um número primo.")
+else:
+    print(f"{num} não é um número primo")
+
+    
